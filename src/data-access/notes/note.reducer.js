1466497bf2a498346e7d17ddd002f420949718c7
Debasish <relaxed.dhinta@gmil.com>
2024-06-19 17:20:06 +0530

    LINT

--- src/data-access/notes/note.reducer.js
+++ src/data-access/notes/note.reducer.js
@@ -1,23 +1,3 @@
-// import { combineReducers } from 'redux';
-// import { ADD_NOTE, DELETE_NOTE } from './note.action';
-
-// const addNoteReducer = (state = [], action) => {
-//   if (action.type === ADD_NOTE) {
-//     return [...state, action.payload];
-//   }
-// };
-
-// const deleteNoteReducer = (state = [], action) => {
-//   if (action.type === DELETE_NOTE) {
-//     return state.filter((note) => note.id !== action.payload);
-//   }
-// };
-
-// export default combineReducers({
-//   addNoteReducer,
-//   deleteNoteReducer,
-// });
-
 import { createSlice } from '@reduxjs/toolkit';
 
 export const notesSlice = createSlice({
---
+++ src/pages/redirect-login/redirect-login.js
@@ -0,0 +1,7 @@
+export function RedirectLogin() {
+  return (
+    <div>
+      <h1>Redirect Login</h1>
+    </div>
+  );
+}
